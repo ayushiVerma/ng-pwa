439d7f6502e4cbbd38323cb26a26de5b22082a1b
Ayushi Verma <ayushi.verma@nagarro.com>
2022-01-12 16:41:52 +0530

    'pwa-update_method_impl'

--- ng-pwa/src/app/employee-list/employee-list.component.ts
+++ ng-pwa/src/app/employee-list/employee-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-employee-list',
@@ -12,9 +13,10 @@ export class EmployeeListComponent implements OnInit {
   employeeList: any;
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private swUpdate: SwUpdate
   ) { 
-
+    this.updateClient();
   }
 
   ngOnInit(): void {
@@ -25,6 +27,26 @@ export class EmployeeListComponent implements OnInit {
       console.log('Error --->', err);
     });
 
+  }
+
+  updateClient(){
+    if(!this.swUpdate.isEnabled){
+      console.log('Not Enaled');
+      return;
+    }
+
+    this.swUpdate.versionUpdates.subscribe(res =>{
+      console.log(`Current `, res);
+      if(confirm('Click OK to update app. New updates available!')){
+        this.swUpdate.activateUpdate().then(
+          () => location.reload()
+        );
+      }
+    });
+
+    this.swUpdate.activated.subscribe(res => {
+      console.log('CUREENT>>>>', res);
+    })
   }
 
 }
